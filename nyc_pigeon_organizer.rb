b67f75c0d65da00596aa70e0f08ed6b2c0f7ed7b
Andre Hochi <andrehochiwork@gmail.com>
2021-02-16 01:49:03 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -11,8 +11,8 @@ def nyc_pigeon_organizer(data)
           final_array[name][key] = []
         end
         final_array[name][key].push(inner_key)
+        end
       end
     end
-  end
   final_results
 end
